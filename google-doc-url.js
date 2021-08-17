33e025b9bfc8a8b4fafc472ddbb646e56faa1783
RecoveryAPP <49496213+RecoveryAPP@users.noreply.github.com>
2021-08-17 13:43:55 -0500

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,5 +1,5 @@
 // paste in your published Google Sheets URL from the browser address bar
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1cVjdp24W-e_KnauQN_3VEmIpopIb8fJr6VzbnWpz8-M/edit#gid=164271551';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1QNIDIkZEdZJ2Y7h6AXw2WMMb4tOWFZFSWG3-o77AnG0/edit#gid=0';
 
 // insert your own Google Sheets API key from https://console.developers.google.com
 var googleApiKey = 'AIzaSyBh9nKnVZm2RPeZa0ywCOxPAgJJfK87WhY';
